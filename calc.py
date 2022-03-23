d97eb9b74389fdc6371ddb95f0c42edeee6bf4c5
luismdsleite <luismdsleite@gmail.com>
2022-03-23 10:46:13 +0000

    Generated data from automated tests

--- calc.py
+++ calc.py
@@ -11,21 +11,22 @@ sizes = []  # x axis for ex 1 and 2
 sizes2 = []  # x axis for ex 2.2 and 3
 # Exercise 1 results
 cTimes1 = []
-cTimes1Misses = []
+cTimes1L1 = []
+cTimes1L2 = []
 javaTimes1 = []
-javaTimes1Misses = []
 # Exercise 2 results
 cTimes2 = []
-cTimes2Misses = []
+cTimes2L1 = []
+cTimes2L2 = []
 javaTimes2 = []
-javaTimes2Misses = []
 # Exercise 2.2 results
 cTimes22 = []
-cTimes22Misses = []
+cTimes22L1 = []
+cTimes22L2 = []
 # Exercise 3 (Dictionary that holds results from diferent block sizes)
 cTimes3 = {}  # Has block sizes as keys and data as values
-cTimes3 = {} 
-cTimes3Misses = {} 
+cTimes3L1 = {}
+cTimes3L2 = {}
 
 # start = 100
 # end = 400+1
@@ -41,7 +42,7 @@ for i in range(start, end, step):
 
 with open(savePath + 'sizes.pkl', 'wb') as f:
     pickle.dump(sizes, f)
-    
+
 print("Started Ex 1")
 # -------------------------- Exercise 1 --------------------------------
 for i in range(start, end, step):
@@ -51,14 +52,31 @@ for i in range(start, end, step):
     c_child_process = subprocess.run(
         ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
 
-    javaTimes1.append(float(java_child_process.stdout.decode(
-        'UTF-8').split("\n")[3].split(" ")[-2]))
-    cTimes1.append(float(c_child_process.stdout.decode(
-        'UTF-8').split("\n")[4].split(" ")[5]))
+    try:
+        # Compiling Java and C++
+        javaTimes1.append(float(java_child_process.stdout.decode(
+            'UTF-8').split("\n")[3].split(" ")[-2]))
+        cTimes1.append(float(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[4].split(" ")[5]))
+
+        # Registering Cache Misses
+        cTimes1L1.append(float(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[7].split(" ")[2]))
+        cTimes1L2.append(str(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[8].split(" ")[2]))
+    except Exception as e:
+        print(e)
 # ----------------------------------------------------------------------
 print("Finished Ex 1")
+
+# Saving Data
 with open(savePath + 'cTimes1.pkl', 'wb') as f:
     pickle.dump(cTimes1, f)
+
+with open(savePath + 'cTimes1L1.pkl', 'wb') as f:
+    pickle.dump(cTimes1L1, f)
+with open(savePath + 'cTimes1L2.pkl', 'wb') as f:
+    pickle.dump(cTimes1L2, f)
 
 with open(savePath + 'javaTimes1.pkl', 'wb') as f:
     pickle.dump(javaTimes1, f)
@@ -72,15 +90,28 @@ for i in range(start, end, step):
     c_child_process = subprocess.run(
         ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
 
-    javaTimes2.append(float(java_child_process.stdout.decode(
-        'UTF-8').split("\n")[3].split(" ")[-2]))
-    cTimes2.append(float(c_child_process.stdout.decode(
-        'UTF-8').split("\n")[4].split(" ")[5]))
+    try:
+        javaTimes2.append(float(java_child_process.stdout.decode(
+            'UTF-8').split("\n")[3].split(" ")[-2]))
+        cTimes2.append(float(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[4].split(" ")[5]))
+
+        cTimes2L1.append(float(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[7].split(" ")[2]))
+        cTimes2L2.append(str(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[8].split(" ")[2]))
+    except Exception as e:
+        print(e)
 # ----------------------------------------------------------------------
 print("Finished Ex 2")
-
+# Saving Data
 with open(savePath + 'cTimes2.pkl', 'wb') as f:
     pickle.dump(cTimes2, f)
+
+with open(savePath + 'cTimes2L1.pkl', 'wb') as f:
+    pickle.dump(cTimes2L1, f)
+with open(savePath + 'cTimes2L2.pkl', 'wb') as f:
+    pickle.dump(cTimes2L2, f)
 
 with open(savePath + 'javaTimes2.pkl', 'wb') as f:
     pickle.dump(javaTimes2, f)
@@ -103,39 +134,68 @@ for i in range(start, end, step):
         ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
 
     # Traceback (most recent call last): File "calc.py", line 74, in <module> cTimes22.append(float(c_child_process.stdout.decode( IndexError: list index out of range
-    cTimes22.append(float(c_child_process.stdout.decode(
-        'UTF-8').split("\n")[4].split(" ")[5]))
+    try:
+        cTimes22.append(float(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[4].split(" ")[5]))
+        cTimes22L1.append(float(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[7].split(" ")[2]))
+        cTimes22L2.append(str(c_child_process.stdout.decode(
+            'UTF-8').split("\n")[8].split(" ")[2]))
+    except Exception as e:
+        print(e)
 # ----------------------------------------------------------------------
 print("Finished Ex 2.2")
 
 with open(savePath + 'cTimes22.pkl', 'wb') as f:
     pickle.dump(cTimes22, f)
 
+with open(savePath + 'cTimes22L1.pkl', 'wb') as f:
+    pickle.dump(cTimes22L1, f)
+with open(savePath + 'cTimes22L2.pkl', 'wb') as f:
+    pickle.dump(cTimes22L2, f)
+
 print("Starting Ex 3")
 # -------------------------- Exercise 3 --------------------------------
 # Defining what block size to use
 bStart = b = 128
 bEnd = 512
+# Initializing Empty Lists
 while b <= bEnd:
     cTimes3[b] = []
+    cTimes3L1[b] = []
+    cTimes3L2[b] = []
     b = b*2
 b = bStart
 
 while b <= bEnd:
     for i in range(start, end, step):
-        if(i%b != 0): # Invalid block size
+        if(i % b != 0):  # Invalid block size
             with open('log', 'a') as f:
-                f.write('Invalid Block Size: i=' + str(i) + ' b=' + str(b)+'\n')
+                f.write('Invalid Block Size: i=' +
+                        str(i) + ' b=' + str(b)+'\n')
             continue
-        print("Start:", i, " Step:", b)  
+        print("Start:", i, " Step:", b)
         input = "3 " + str(i) + " " + str(b)
         c_child_process = subprocess.run(
             ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
-        cTimes3[b].append(float(c_child_process.stdout.decode(
-            'UTF-8').split("\n")[4].split(" ")[7]))
+        try:
+            cTimes3[b].append(float(c_child_process.stdout.decode(
+                'UTF-8').split("\n")[4].split(" ")[7]))
+
+            cTimes3L1[b].append(float(c_child_process.stdout.decode(
+                'UTF-8').split("\n")[7].split(" ")[2]))
+            cTimes3L2[b].append(str(c_child_process.stdout.decode(
+                'UTF-8').split("\n")[8].split(" ")[2]))
+        except Exception as e:
+            print(e)
     b = b*2
 # ----------------------------------------------------------------------
 print("Finished Ex 3")
-
+# Saving Data
 with open(savePath + 'cTimes3.pkl', 'wb') as f:
     pickle.dump(cTimes3, f)
+
+with open(savePath + 'cTimes3L1.pkl', 'wb') as f:
+    pickle.dump(cTimes3L1, f)
+with open(savePath + 'cTimes3L2.pkl', 'wb') as f:
+    pickle.dump(cTimes3L2, f)
--- plot.py
+++ plot.py
@@ -3,92 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-# # Matrix sizes used
-# sizes = []  # x axis for ex 1 and 2
-# sizes2 = []  # x axis for ex 2.2 and 3
-# # Exercise 1 results
-# cTimes1 = []
-# javaTimes1 = []
-# # Exercise 2 results
-# cTimes2 = []
-# javaTimes2 = []
-# # Exercise 2.2 results
-# cTimes22 = []
-# # Exercise 3 (Dictionary that holds results from diferent block sizes)
-# cTimes3 = {}  # Has block sizes as keys and data as values
-
-# start = 100
-# end = 400+1
-# step = 50
-
-# for i in range(start, end, step):
-#     sizes.append(int(i))
-
-# # -------------------------- Exercise 1 --------------------------------
-# for i in range(start, end, step):
-#     input = "1 " + str(i)
-#     java_child_process = subprocess.run(
-#         ['java', 'Matrix'], stdout=subprocess.PIPE, input=input.encode())
-#     c_child_process = subprocess.run(
-#         ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
-
-#     javaTimes1.append(float(java_child_process.stdout.decode(
-#         'UTF-8').split("\n")[3].split(" ")[-2]))
-#     cTimes1.append(float(c_child_process.stdout.decode(
-#         'UTF-8').split("\n")[4].split(" ")[5]))
-# # ----------------------------------------------------------------------
-
-# # -------------------------- Exercise 2 --------------------------------
-# for i in range(start, end, step):
-#     input = "2 " + str(i)
-#     java_child_process = subprocess.run(
-#         ['java', 'Matrix'], stdout=subprocess.PIPE, input=input.encode())
-#     c_child_process = subprocess.run(
-#         ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
-
-#     javaTimes2.append(float(java_child_process.stdout.decode(
-#         'UTF-8').split("\n")[3].split(" ")[-2]))
-#     cTimes2.append(float(c_child_process.stdout.decode(
-#         'UTF-8').split("\n")[4].split(" ")[5]))
-# # ----------------------------------------------------------------------
-
-# # Exercise 2.2 and 3 user diferent ranges for the matrix size
-# start = 200
-# end = 600+1
-# step = 50
-# for i in range(start, end, step):
-#     sizes2.append(int(i))
-
-# # -------------------------- Exercise 2.2 --------------------------------
-# for i in range(start, end, step):
-#     input = "2 " + str(i)
-#     c_child_process = subprocess.run(
-#         ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
-
-#     cTimes22.append(float(c_child_process.stdout.decode(
-#         'UTF-8').split("\n")[4].split(" ")[5]))
-# # ----------------------------------------------------------------------
-
-# # -------------------------- Exercise 3 --------------------------------
-# # Defining what block size to use
-# bStart = b = 25
-# bEnd = 50
-# while b <= bEnd:
-#     cTimes3[b] = []
-#     b = b*2
-# b = bStart
-
-# while b <= bEnd:
-#     for i in range(start, end, step):
-#         input = "3 " + str(i) + " " + str(b)
-#         c_child_process = subprocess.run(
-#             ['./matrix'], stdout=subprocess.PIPE, input=(input+" 0").encode())
-#         cTimes3[b].append(float(c_child_process.stdout.decode(
-#             'UTF-8').split("\n")[4].split(" ")[7]))
-#     b = b*2
-# # ----------------------------------------------------------------------
-
-
 def plotAndSave(x, ys, legend, title, saveName):
     # Plot graph
     for y in ys:
@@ -113,17 +27,32 @@ def plotAndSave(x, ys, legend, title, saveName):
 loadPath = "./pickle/"
 savePath = "./graphs/"
 
+# Loading List of sizes used
 with open(loadPath + 'sizes.pkl', 'rb') as f:
     sizes = pickle.load(f)
 
+# Normal Multiplication Results (C++)
 with open(loadPath + 'cTimes1.pkl', 'rb') as f:
     cTimes1 = pickle.load(f)
 
+# Normal Multiplication Misses (C++)
+with open(loadPath + 'cTimes1L1.pkl', 'rb') as f:
+    cTimes1L1 = pickle.load(f)
+with open(loadPath + 'cTimes1L2.pkl', 'rb') as f:
+    cTimes1L2 = pickle.load(f)
+
+# Normal Multiplication Results (Java)
 with open(loadPath + 'javaTimes1.pkl', 'rb') as f:
     javaTimes1 = pickle.load(f)
 
+# Line Multiplication Results (C++)
 with open(loadPath + 'cTimes2.pkl', 'rb') as f:
     cTimes2 = pickle.load(f)
+
+with open(loadPath + 'cTimes2L1.pkl', 'rb') as f:
+    cTimes2L1 = pickle.load(f)
+with open(loadPath + 'cTimes2L2.pkl', 'rb') as f:
+    cTimes2L2 = pickle.load(f)
 
 with open(loadPath + 'javaTimes2.pkl', 'rb') as f:
     javaTimes2 = pickle.load(f)
@@ -131,15 +60,22 @@ with open(loadPath + 'javaTimes2.pkl', 'rb') as f:
 with open(loadPath + 'sizes2.pkl', 'rb') as f:
     sizes2 = pickle.load(f)
 
-# with open(loadPath + 'cTimes22.pkl', 'rb') as f:
-#     cTimes22 = pickle.load(f)
+with open(loadPath + 'cTimes22.pkl', 'rb') as f:
+    cTimes22 = pickle.load(f)
 
-# with open(loadPath + 'cTimes3.pkl', 'rb') as f:
-#     cTimes3 = pickle.load(f)
+with open(loadPath + 'cTimes3.pkl', 'rb') as f:
+    cTimes3 = pickle.load(f)
+
+with open(loadPath + 'cTimes3L1.pkl', 'rb') as f:
+    cTimes3L1 = pickle.load(f)
+with open(loadPath + 'cTimes3L2.pkl', 'rb') as f:
+    cTimes3L2 = pickle.load(f)
 
 print(sizes)
 print(sizes2)
 print(cTimes1)
+print(cTimes1L1)
+print(cTimes1L2)
 # print(cTimes3)
 
 # -------------------------- Exercise 1/2 ------------------------------
@@ -156,8 +92,8 @@ plotAndSave(sizes, [cTimes1, cTimes2],
             ['Normal Mult', 'Line Mult'], 'Normal Multiplication vs Line Multiplication (C++)', savePath+'c_comparison.png')
 
 # -------------------------- Exercise 2.2/3 ----------------------------
-# plotAndSave(sizes2, cTimes3.values(),
-#             ["Block Size="+str(b) for b in cTimes3.keys()], 'Block Multiplication (C++)', savePath+'ex3.png')
+plotAndSave(sizes2, cTimes3.values(),
+            ["Block Size="+str(b) for b in cTimes3.keys()], 'Block Multiplication (C++)', savePath+'ex3.png')
 
-# plotAndSave(sizes2, [cTimes22] + list(cTimes3.values()),
-#             ['Line Mult'] + ["Block Mult (b="+str(b)+")" for b in cTimes3.keys()], 'Block Multiplication vs Line Multiplication (C++)', savePath+'ex3And2_2.png')
+plotAndSave(sizes2, [cTimes22] + list(cTimes3.values()),
+            ['Line Mult'] + ["Block Mult (b="+str(b)+")" for b in cTimes3.keys()], 'Block Multiplication vs Line Multiplication (C++)', savePath+'ex3And2_2.png')
